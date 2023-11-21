01a14d1e643225b5f8df6e22e138d7c659569d34
Chin <86007652+Chin0303@users.noreply.github.com>
2023-11-21 17:22:06 +0100

    :D

--- Monke Dimensions/DimensionController.cs
+++ Monke Dimensions/DimensionController.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,8 +9,6 @@ namespace Monke_Dimensions
 {
     internal class DimensionController
     {
-        // https://stackoverflow.com/questions/20520238
-
         public static async Task LoadDimensions()
         {
             Debug.Log("-> Loaded Dimension(s): <-");
@@ -25,26 +22,51 @@ namespace Monke_Dimensions
 
                 using (var zip = ZipFile.OpenRead(currentPath))
                 {
-
                     ZipArchiveEntry packageEntry = zip.GetEntry("Package.json");
-                    ZipArchiveEntry bundleEntry = zip.Entries.FirstOrDefault(entry => string.IsNullOrEmpty(Path.GetExtension(entry.Name)));
 
-                    if (bundleEntry == null || packageEntry == null)
+                    if(packageEntry == null)
                     {
-                        Debug.LogError("Invalid dimension: " + currentPath);
+                        Debug.LogError("Fuck you: " + currentPath); // if people decide to mess with the json
                         continue;
                     }
 
-                    using (StreamReader bundleReader = new StreamReader(bundleEntry.Open()))
-
-                    using (StreamReader packageReader = new StreamReader(packageEntry.Open()))
-                    {
-                        DimensionPackage package = Newtonsoft.Json.JsonConvert.DeserializeObject<DimensionPackage>(packageReader.ReadToEnd());
-                        Debug.Log($"-> Name: {package.Name}, Author: {package.Author} <-");
-                    }
+                    await LoadAndInstantiateAssets(dimensionFile);
                 }
             }
-            await Task.Yield(); // Stupid warning >:(
+            await Task.Yield();
+        }
+
+
+        /* I've spend 1.5 days only doing this, i couldve played fortnite :( */
+        internal static async Task LoadAndInstantiateAssets(string zipFilePath)
+        {
+            byte[] zipBytes = File.ReadAllBytes(zipFilePath);
+            byte[] bundleBytes;
+
+            using (MemoryStream zipStream = new MemoryStream(zipBytes))
+            using (ZipArchive zipArchive = new ZipArchive(zipStream, ZipArchiveMode.Read))
+            {
+                ZipArchiveEntry selectedEntry = zipArchive.Entries.FirstOrDefault(entry => string.IsNullOrEmpty(Path.GetExtension(entry.Name)));
+
+                using (Stream entryStream = selectedEntry.Open())
+                using (MemoryStream bundleStream = new MemoryStream())
+                {
+                    await entryStream.CopyToAsync(bundleStream);
+                    bundleBytes = bundleStream.ToArray();
+                }
+            }
+
+            AssetBundleCreateRequest assetBundleCreateRequest = AssetBundle.LoadFromMemoryAsync(bundleBytes);
+
+            AssetBundle assetBundle = assetBundleCreateRequest.assetBundle;
+
+            GameObject[] gameObjects = assetBundle.LoadAllAssets<GameObject>();
+
+            foreach (GameObject meow in gameObjects)
+            {
+                Debug.Log(meow.name);
+                GameObject instantiatedObject = Object.Instantiate(meow); // will do better later only for testing
+            }
         }
     }
 }
